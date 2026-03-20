efa30b25f15c9f8b0c39464ad8887d804562bf84
tricia <e1355062@u.nus.edu>
2026-03-20 23:27:18 +0800

    Add missing implements clause

--- src/main/java/seedu/address/logic/parser/DeleteOrderCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteOrderCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new DeleteOrderCommand object
  */
-public class DeleteOrderCommandParser {
+public class DeleteOrderCommandParser implements Parser<DeleteOrderCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteOrderCommand
