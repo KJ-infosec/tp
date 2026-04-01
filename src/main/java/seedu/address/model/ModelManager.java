3762ddc771486ed02ba7f899a817880fbfdd0ea5
tricia <e1355062@u.nus.edu>
2026-04-01 09:56:39 +0800

    Filter orders on app startup

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -38,6 +38,12 @@ public class ModelManager implements Model {
         this.userPrefs = new UserPrefs(userPrefs);
         filteredPersons = new FilteredList<>(this.addressBook.getPersonList());
         filteredOrders = new FilteredList<>(this.addressBook.getOrderList());
+
+        // Startup filter: show only PREPARING + READY orders
+        filteredOrders.setPredicate(order ->
+                order.getStatus().value.equals("PREPARING")
+                        || order.getStatus().value.equals("READY")
+        );
     }
 
     public ModelManager() {
